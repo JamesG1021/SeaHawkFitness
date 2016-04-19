213b927628eca5c1bea5ec3af1ed9a9afdc004fb
James Gray <jsg6998@uncw.edu>
2016-04-19 14:57:29 -0400

    layout edits
    more layout edits

---
+++ Database Source and Population /CreateSHFitnessDB.sql
@@ -0,0 +1,196 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+drop table IF EXISTS equipment;
+drop table IF EXISTS rents;
+drop table IF EXISTS students;
+drop table IF EXISTS admin;
+drop table IF EXISTS trainers;
+drop table IF EXISTS trains;
+drop table IF EXISTS takes;
+drop table IF EXISTS courses;
+drop table IF EXISTS instructs;
+drop table IF EXISTS events;
+drop table IF EXISTS attending;
+drop table IF EXISTS sa_events;
+drop table IF EXISTS instructs_events;
+
+create table equipment(
+  equipID integer(6),
+  equipName varchar(20),
+  equipPrice integer(6),
+  primary key(equipID)) engine = innodb;
+
+create table rents(
+  equipID integer(6),
+  studentID integer(9),
+  primary key(equipID, studentID))
+  engine = innodb;
+
+alter table rents
+add constraint eID_ibfk_1 foreign key(equipID) references equipment(equipID) ON DELETE CASCADE ON UPDATE CASCADE;
+
+create table students(
+  studentID integer(9),
+  name varchar(40),
+  primary key(studentID)) engine = innodb;
+
+alter table rents
+add constraint sID_ibfk_1 foreign key(studentID) references students(studentID) ON DELETE CASCADE ON UPDATE CASCADE;
+
+create table trainers(
+   instructorID integer(9),
+   name varchar(40),
+   primary key(instructorID)) engine = innodb;
+
+create table admin(
+  instructorID integer(9),
+  name varchar(40),
+  primary key(instructorID)) engine = inodb;
+
+create table trains(
+   instructorID integer(9),
+   studentID integer(9),
+   primary key(instructorID, studentID)) engine = innodb;
+
+alter table trains
+add constraint sID_ibfk_2 foreign key(studentID) references students(studentID) ON DELETE CASCADE ON UPDATE CASCADE;
+
+alter table trains
+add constraint iID_ibfk_1 foreign key(instructorID) references trainers(instructorID) ON DELETE CASCADE ON UPDATE CASCADE;
+
+create table takes(
+  studentID integer(9),
+  courseID integer(6),
+  primary key(studentID, courseID)) engine = innodb;
+
+alter table takes
+add constraint sID_ibfk_3 foreign key(studentID) references students(studentID) ON DELETE CASCADE ON UPDATE CASCADE;
+
+create table courses(
+  courseID integer(6),
+  courseName varchar(40),
+  startTime varchar(6),
+  endTime varchar(6),
+  studio varchar(80),
+  description varchar(255),
+  day varchar(10),
+  primary key(courseID)) engine = innodb;
+/* some weirdness in this table on the data dictionary*/
+
+alter table takes
+add constraint cID_ibfk_1 foreign key(courseID) references courses(courseID) ON DELETE CASCADE ON UPDATE CASCADE;
+
+create table instructs(
+  instructorID integer(9),
+  courseID integer(6),
+  primary key(instructorID, courseID)) engine = innodb;
+
+alter table instructs
+add constraint cID_ibfk_2 foreign key(courseID) references courses(courseID) ON DELETE CASCADE ON UPDATE CASCADE;
+alter table instructs
+add constraint iID_ibfk_3 foreign key(instructorID) references trainers(instructorID) ON DELETE CASCADE ON UPDATE CASCADE;
+
+create table events(
+  eventID integer(6),
+  time varchar(6),
+  startDate varchar(10),
+  endDate varchar(10),
+  description varchar(255),     ## Description added to events
+  eventName varchar(10),
+  primary key(eventID)) engine = innodb;
+
+create table instructs_events (
+  eventID integer(6),
+  instructorID integer(9),
+  primary key(eventID, instructorID)) engine = innodb;
+
+alter table instructs_events
+add constraint iID_ibfk_6 foreign key(instructorID) references trainers(instructorID) ON DELETE CASCADE ON UPDATE CASCADE;
+
+alter table instructs_events
+add constraint iID_ibfk_7 foreign key(eventID) references events(eventID) ON DELETE CASCADE ON UPDATE CASCADE;
+
+delimiter #
+create trigger add_sa_events after insert on events
+  for each row
+  BEGIN
+  insert into sa_events(adventureID, name, startDate, endDate, time, description) values(NEW.eventID, NEW.eventName, NEW.startDate, NEW.endDate, NEW.time, NEW.description);
+    end#
+    delimiter ;
+
+
+create table sa_events(
+  adventureID integer(6),
+  name varchar(30),
+  startDate varchar(10),
+  endDate varchar(10),
+  time varchar(10),
+  description varchar(255),     ## Description added to sa_events
+  instructorID integer(9),
+  enrolled integer(5) DEFAULT 0,  ## Added enrolled to check against capacity
+  capacity integer(5),
+  price integer(5),
+  primary key(adventureID)) engine = innodb;
+/* some weirdness on the data dictionary for this one*/
+
+alter table sa_events
+add constraint iID_ibfk_5 foreign key(instructorID) references trainers(instructorID);
+
+
+create table attending(
+  studentID integer(9),
+  day varchar(10),
+  time varchar(6),
+  primary key(studentID, day)) engine = innodb;
+
+alter table attending
+add constraint sID_ibfk_4 foreign key(studentID) references students(studentID) ON DELETE CASCADE ON UPDATE CASCADE;
+
+#views
+create view trainer_name as
+select distinct name from trainers;
+#views
+create view rental_name as
+select distinct name from equipment;
+
+#intersect
+create view rented_name as
+select * from equipment where equipment.equipID in (select equipID from rents);
+
+#aggregate function average
+create view avg_capacity as
+select avg(capacity) from sa_events;
+
+#nested subquerry and stuff
+create view students_renting as
+select * from students where exists (select * from rents where rents.studentID = students.studentID);
+
+#stored function example
+DELIMITER $$
+CREATE FUNCTION valid_id(id_number integer) RETURNS VARCHAR(10)
+    DETERMINISTIC
+BEGIN
+    DECLARE valid varchar(10);
+
+    IF id_number > 950123455 THEN
+        SET valid = 'FALSE';
+    ELSE
+        SET valid = 'TRUE';
+    END IF;
+
+ RETURN (valid);
+END $$
+DELIMITER ;
+
+#stored function example
+select name, valid_id(instructorID) from trainers ORDER BY name;
+
+#stored procedure
+DELIMITER $$
+
+CREATE PROCEDURE totalparticipants
+(OUT total int)
+BEGIN
+    select sum(enrolled) from sa_events;
+  END $$
+DELIMITER ;
---
+++ Database Source and Population /PopulateSHFitnessDB.sql
@@ -0,0 +1,232 @@
+
+insert into admin values
+(123456789, 'SFAdmin');
+
+insert into students values
+(950000001, 'James');
+insert into students values
+(950000002, 'Amanda');
+insert into students values
+(950000003, 'Sean');
+insert into students values
+(950000004, 'Ben');
+insert into students values
+(950000005, 'Bryan');
+insert into students values
+(950000006, 'Josh');
+insert into students values
+(950000007, 'Tess');
+insert into students values
+(950000008, 'Joshua');
+insert into students values
+(950000009, 'Megan');
+insert into students values
+(950000010, 'Lora');
+insert into students values
+(950000011, 'Amber');
+insert into students values
+(950000012, 'Jake');
+insert into students values
+(950000013, 'Jeff');
+insert into students values
+(950000014, 'Dill');
+insert into students values
+(950000015, 'Laura');
+insert into students values
+(950000016, 'Lin');
+insert into students values
+(950000017, 'Daniel');
+insert into students values
+(950000018, 'Mitch');
+insert into students values
+(950000019, 'Channing');
+insert into students values
+(950000020, 'Bob');
+
+insert into trainers values
+(950123450, 'Mohammad');
+insert into trainers values
+(950123451, 'Ghandi');
+insert into trainers values
+(950123452, 'Benjamin');
+insert into trainers values
+(950123453, 'Tesla');
+insert into trainers values
+(950123454, 'Minoto');
+insert into trainers values
+(950123455, 'Obama');
+insert into trainers values
+(950123456, 'Mozart');
+insert into trainers values
+(950123457, 'Bach');
+insert into trainers values
+(950123458, 'Beehtoven');
+insert into trainers values
+(950123459, 'Einstien');
+
+insert into trains values
+(950123450,950000001);
+insert into trains values
+(950123450,950000002);
+insert into trains values
+(950123451,950000003);
+insert into trains values
+(950123451,950000004);
+insert into trains values
+(950123452,950000005);
+insert into trains values
+(950123452,950000006);
+insert into trains values
+(950123453,950000007);
+insert into trains values
+(950123453,950000008);
+insert into trains values
+(950123454,950000009);
+insert into trains values
+(950123454,950000010);
+insert into trains values
+(950123455,950000011);
+insert into trains values
+(950123455,950000012);
+insert into trains values
+(950123456,950000013);
+insert into trains values
+(950123456,950000014);
+insert into trains values
+(950123457,950000015);
+insert into trains values
+(950123457,950000016);
+insert into trains values
+(950123458,950000017);
+insert into trains values
+(950123458,950000018);
+insert into trains values
+(950123459,950000019);
+insert into trains values
+(950123459,950000020);
+
+insert into equipment values
+(850120, 'Kayak', 19);
+insert into equipment values
+(850121, 'Kayak', 19);
+insert into equipment values
+(850122, 'Tent', 15);
+insert into equipment values
+(850123, 'Tent', 15);
+insert into equipment values
+(850124, 'Canoe', 24);
+insert into equipment values
+(850125, 'Canoe', 24);
+insert into equipment values
+(850126, 'Corn Hole', 12);
+insert into equipment values
+(850127, 'Corn Hole', 12);
+insert into equipment values
+(850128, 'Sleeping Bag', 9);
+insert into equipment values
+(850129, 'Grill', 14);
+
+insert into rents values
+(850120,950000002);
+insert into rents values
+(850122,950000004);
+insert into rents values
+(850124,950000006);
+insert into rents values
+(850126,950000008);
+insert into rents values
+(850128,950000010);
+
+insert into courses values
+(123450,'Yoga Flow','9:30am','10:30am','Studio 129','A new form of Yoga that will get the energy flowing.', 'Monday');
+insert into courses values
+(123451,'Core','8:00am','9:00am','Studio 226/228','Blast your abdominals with this half hour hell.','Tuesday');
+insert into courses values
+(123452,'Yoga relax','9:00am','10:00am','Studio 129','Relax and unwind with this meditative soothing Yoga session.','Wednesday');
+insert into courses values
+(123453,'Yogalates','2:00pm','3:00pm','Studio 129','A pleasant and exhilerating blending of Yoga and Pilates.','Thursday');
+insert into courses values
+(123454,'Cardio Dance','4:00pm','5:00pm','Studio 226/228','An upbeat and aggresive Cardio Dance session,  get ready to sweat!','Friday');
+insert into courses values
+(123455,'Body Pump','2:00pm','3:00pm','Studio 226/228','Engage your whole body in this vigarous workout.','Monday');
+insert into courses values
+(123456,'Pilates','8:00am','9:00am','Studio 129','You love it, we love it, Pilates.','Tuesday');
+insert into courses values
+(123457,'Yoga Relax','3:00pm','4:00pm','Studio 129','Relax and unwind with this meditative soothing Yoga session.','Wednesday');
+insert into courses values
+(123458,'Cycle30','9:00am','10:00am','Studio 223','30 minutes of intense bicycling action!','Thursday');
+insert into courses values
+(123459,'Hydrofit','1:00pm','2:00pm','Indoor Pool','Low impact aquatic exercise fun!','Friday');
+
+insert into instructs values
+(950123450,123450);
+insert into instructs values
+(950123451,123451);
+insert into instructs values
+(950123452,123452);
+insert into instructs values
+(950123453,123453);
+insert into instructs values
+(950123454,123454);
+insert into instructs values
+(950123455,123455);
+insert into instructs values
+(950123456,123456);
+insert into instructs values
+(950123457,123457);
+insert into instructs values
+(950123458,123458);
+insert into instructs values
+(950123459,123459);
+
+insert into takes values
+(950000001,123450);
+insert into takes values
+(950000003,123451);
+insert into takes values
+(950000005,123452);
+insert into takes values
+(950000007,123453);
+insert into takes values
+(950000009,123454);
+insert into takes values
+(950000011,123455);
+insert into takes values
+(950000013,123456);
+insert into takes values
+(950000015,123457);
+insert into takes values
+(950000017,123458);
+insert into takes values
+(950000019,123459);
+
+insert into events values
+(890120,'8:00pm','07-12-16', '07-12-16','High paced hip hop and latin dance fitness.','Zumba');
+insert into events values
+(890121,'9:00am','05-25-16','05-25-16','Get on the fast track to that beach body with our first time offering of Insanity!', 'Insanity');
+insert into events values
+(890122,'8:00am','04-22-16','04-22-16','Blast fat away with P90x',  'P90x');
+
+insert into sa_events values
+(758200,'Kayaking Trip','04-05-16', '04-10-16','6:00pm','Kayak in the Everglades',950123459, 10,20,50);
+insert into sa_events values
+(758210,'Camping Trip','03-21-16', '03-28-16','5:00pm','Come join us for camping in the Appalachian Mountains',950123458, 10, 15,75);
+
+insert into attending values
+(950000020, '04-05-16', '8:00am');
+insert into attending values
+(950000018, '04-05-16', '8:00am');
+insert into attending values
+(950000016, '04-05-16', '8:00am');
+insert into attending values
+(950000014, '04-05-16', '8:00am');
+insert into attending values
+(950000012, '03-21-16', '9:00am');
+insert into attending values
+(950000010, '03-21-16', '9:00am');
+insert into attending values
+(950000008, '03-21-16', '9:00am');
+insert into attending values
+(950000006, '03-21-16', '9:00am');
+
+SET FOREIGN_KEY_CHECKS=1;
