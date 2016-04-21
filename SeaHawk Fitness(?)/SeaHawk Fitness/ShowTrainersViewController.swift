aae4add26532d2d7d0f4fd37a9ceaa8ab8f0f1c1
Amanda Hope Harman <amandahharman@gmail.com>
2016-04-20 22:48:17 -0400

    Add Trainer
    Added the insertion functionality for trainer

--- SeaHawk Fitness(?)/SeaHawk Fitness/ShowTrainersViewController.swift
+++ SeaHawk Fitness(?)/SeaHawk Fitness/ShowTrainersViewController.swift
@@ -8,14 +8,50 @@
 
 import UIKit
 
-class ShowTrainersViewController: UIViewController {
+class ShowTrainersCell : UITableViewCell{
+    
+
+    @IBOutlet weak var instructorID: UILabel!
+    @IBOutlet weak var instructorName: UILabel!
+    
+    func setupCell( instructorId: String!, name: String!){
+        instructorID.text = instructorId
+        instructorName.text = name
+
+        self.contentView.clipsToBounds = true;
+    }
+}
+
+
+class ShowTrainersViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
 
     
     @IBOutlet weak var tableView: UITableView!
+    
+    let trainersAPI = "TrainersService"
+    
+    var RequestARGs = ""
+    var EditARGs = ""
+    
+    var items = [Instructor]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    override func viewWillAppear(animated: Bool) {
+        
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
+        let nib = UINib(nibName:"ShowTrainersCell", bundle: nil)
+        tableView.registerNib(nib, forCellReuseIdentifier: "ShowTrainersCell")
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "refreshList:", name:"refreshMyData", object: nil)
+        
+        InstructorsItems.removeAll()
+        getTrainers()
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,6 +59,47 @@ class ShowTrainersViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return InstructorsItems.count
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: ShowTrainersCell = (tableView.dequeueReusableCellWithIdentifier("ShowTrainersCell") as? ShowTrainersCell)!
+        
+        let trainer = InstructorsItems[indexPath.row]
+        
+        cell.setupCell(String(trainer.instructorID), name: trainer.name)
+        
+        let backgroundView = UIView()
+        backgroundView.backgroundColor = UIColor.cyanColor()
+        cell.selectedBackgroundView = backgroundView
+        return cell
+    }
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let currentCell = tableView.cellForRowAtIndexPath(indexPath) as! ShowTrainersCell
+        tableView.beginUpdates()
+        print(currentCell.instructorName.text)
+        tableView.endUpdates()
+    }
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    func getTrainers(){
+        
+        makeDatabaseRequest(self.view, API: trainersAPI, EditARGs: EditARGs, RequestARGs: RequestARGs)
+        
+    }
+    
+    func refreshList(notification: NSNotification){
+        
+        self.tableView.reloadData()
+    }
+    
+
 
     /*
     // MARK: - Navigation
